516264dcb307aff8cdd154e67dae7cb816788bca
nadideozturk <nadidebaca@gmail.com>
2018-12-24 22:13:40 -0800

    Intial files, homemade and outside meal pages added.

---
+++ src/_nav.js
@@ -0,0 +1,32 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+    {
+      title: true,
+      name: 'Meals',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Homemade',
+      url: '/homemademeal',
+      icon: 'icon-drop',
+    },
+    {
+      name: 'Outside',
+      url: '/outsidemeal',
+      icon: 'icon-pencil',
+    },
+  ],
+};
